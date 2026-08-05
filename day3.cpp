aa9ada0aa262bc0395da1aa8ded344fb912848cf
techhviaangel <av1634070@gmail.com>
2026-08-05 21:01:12 +0530

    Day 3 completed

---
+++ day3.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+     int a;
+     int b;
+
+     cout<<"Enter two integers:";
+     cin>>a>>b;
+
+     cout<<"Sum="<<a+b;
+     cout<<"Difference="<<a-b;
+     cout<< "Product="<<a*b;gi
+     cout<<"Quotient="<<a/b; 
+
+return 0;
+}
