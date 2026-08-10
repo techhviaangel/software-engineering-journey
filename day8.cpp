f85babc530b0861c9334cfbfd4e6ceb313bbfacb
techhviaangel <av1634070@gmail.com>
2026-08-10 12:23:46 +0530

    Day 8: Added for loop practice

---
+++ day8.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int i;
+    // for loop for writing numbers from 20-1
+
+    for(i=0; i<=10 ; i++)
+    {
+        cout<<i<<endl; }
+     return 0;
+}
