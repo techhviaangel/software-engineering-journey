b8289f202afc94750e76456b28ae6defd81cce48
techhviaangel <av1634070@gmail.com>
2026-08-16 10:40:31 +0530

    Add Day 12 C++ and accounting problems

---
+++ day12.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+   int n;
+   int sum=0;
+
+   cin>>n;
+   for(int i=1; i<=n ; i++){
+    sum=sum+i;
+   }
+   cout<<"Sum="<<sum;
+   return 0;
+}
